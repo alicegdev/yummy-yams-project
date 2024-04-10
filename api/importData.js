2a4610cc606082dba7c4063e0c7db64c6dfb22a2
Alice Goarnisson <alice.goarnisson@gmail.com>
2024-04-10 13:33:23 +0200

    added import Data script to writable folder

---
+++ api/importData.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const { MongoClient } = require("mongodb");
+
+async function main() {
+  const uri = "mongodb://mongo:27017"; // URL de connexion à MongoDB
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Sélectionner la base de données mydatabase
+    const db = client.db("yummy_yams_db");
+
+  try {
+    await client.connect();
+    console.log("Connecté à MongoDB");
+
+    const files = fs.readdirSync("./data");
+    for (const file of files) {
+      if (fs.statSync(`./data/${file}`).size === 0) {
+        console.log(`Le fichier ${file} est vide. Passer au suivant.`);
+        continue;
+      }
+
+      const data = JSON.parse(fs.readFileSync(`./data/${file}`, "utf8"));
+      const collectionName = file.split(".")[0];
+      const collection = client.db("mydatabase").collection(collectionName);
+      await collection.insertMany(data);
+      console.log(`Données insérées dans la collection ${collectionName}`);
+    }
+  } catch (error) {
+    console.error("Erreur lors de l'importation des données :", error);
+  } finally {
+    await client.close();
+  }
+}
+
+main();
