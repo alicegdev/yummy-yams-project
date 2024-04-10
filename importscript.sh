56c35412446d88a55700db8e0a5b14d6ecdd9f2a
Alice Goarnisson <alice.goarnisson@gmail.com>
2024-04-10 15:15:34 +0200

    added two tables creation in bash

--- importscript.sh
+++ importscript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 mongoimport --db='yummy-yams-db' --collection='pastries' --file='/tmp/pastries.json' --jsonArray --username='root' --password='foobar' --authenticationDatabase=admin
+
+# Créer des collections supplémentaires
+mongo --eval "db.createCollection('users')"
+mongo --eval "db.createCollection('winners')"
