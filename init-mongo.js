ee590b2d2e5b34d2d2d4a9e5cd77f9577e03d5f5
Alice Goarnisson <alice.goarnisson@gmail.com>
2024-04-18 16:51:17 +0200

    added a createUser

--- init-mongo.js
+++ init-mongo.js
@@ -1,2 +1,9 @@
 db = db.getSiblingDB('yummy-yams-db')
 db.createCollection('pastries')
+yummyYamsDB.createUser({
+    user: 'root',
+    pwd: 'foobar',
+    roles: [
+        { role: 'readWrite', db: 'yummy-yams-db' }
+    ]
+});
